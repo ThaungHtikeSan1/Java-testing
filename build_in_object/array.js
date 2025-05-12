dc56b46fb41373c14be589ec29bbe969546bde78
ThaungHtikeSan1 <khantnyaroo12@gmail.com>
2025-05-12 17:08:29 +0630

    add array

--- build_in_object/array.js
+++ build_in_object/array.js
@@ -1,34 +1,84 @@
-const fruits = ["apple", "banana", "orange", "grape", "mango"];
+// const fruits = ["apple", "banana", "orange", "grape", "mango"];
 
-console.dir(Array);
-console.log(fruits);
+// console.dir(Array);
+// console.log(fruits);
 
-//toString==> Arrary to String
-console.log(fruits.toString());
+// //toString==> Arrary to String
+// console.log(fruits.toString());
 
-//join==> Array to String("==")
-console.log(fruits.join("***"));
+// //join==> Array to String("==")
+// console.log(fruits.join("***"));
 
-//sort ==> A Z to a z
-console.log(fruits.sort());
+// //sort ==> A Z to a z
+// console.log(fruits.sort());
 
-//reverse ==> lastIndex to firstIndex
-console.log(fruits.reverse());
+// //reverse ==> lastIndex to firstIndex
+// console.log(fruits.reverse());
 
-//push, pop ==> (last in/out)
-console.log(fruits.push("DragonFruit"));
-console.log(fruits);
-fruits.push("chilies")
-console.log(fruits);
-fruits.pop()
-console.log(fruits);
+// //push, pop ==> (last in/out)
+// console.log(fruits.push("DragonFruit"));
+// console.log(fruits);
+// fruits.push("chilies")
+// console.log(fruits);
+// fruits.pop()
+// console.log(fruits);
 
-//unshift/shift ==> (first in/out)
-console.log(fruits.unshift("pineapple"));
-console.log(fruits);
-fruits.unshift("Watermelon")
-console.log(fruits);
+// //unshift/shift ==> (first in/out)
+// console.log(fruits.unshift("pineapple"));
+// console.log(fruits);
+// fruits.unshift("Watermelon")
+// console.log(fruits);
 
-fruits.shift()
-console.log(fruits);
+// fruits.shift()
+// console.log(fruits);
+
+// // isArray
+// console.log(Array.isArray([]));
+// console.log(Array.isArray(fruits));
+
+// // includes
+// console.log(fruits.includes("apple"));
+// console.log(fruits.includes("Durin"));
+
+// //indexof
+// console.log(fruits.indexOf("grape"));
+// console.log(fruits.indexOf("Durin"));
+
+// //slice => cut of array
+// console.log(fruits.slice(2,5));
+// console.log(fruits);
+
+// //splice => cut off array
+// console.log(fruits.splice(4,3));
+// console.log(fruits);
+
+
+// numbers = [10, 20, 30, 40, 50]
+
+// jungle = ["apple", "banana", "cherry"]
+
+// person = ["John", "Leo", "Thiago"]
+
+// // concat => combine array
+// const finalArray = numbers.concat(jungle).concat(person)
+// console.log(finalArray);
+
+//spread => array and object combine
+// const obj1={
+//     a:"aa",
+//     b:"bb",
+//     c:"cc"
+// }
+
+// const obj2={
+//     x:"xx",
+//     y:"yy",
+//     z:"zz"
+// }
+
+// // const finalArray = [...numbers,...jungle,...person]
+// // console.log(finalArray);
+
+// const finalObj = {...obj1,...obj2}
+// console.log(finalObj);
 
