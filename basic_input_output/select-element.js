e550ff96bf41675fb1f492f273a08281cf6779a7
ThaungHtikeSan1 <khantnyaroo12@gmail.com>
2025-05-23 23:32:39 +0630

    select_element

--- basic_input_output/select-element.js
+++ basic_input_output/select-element.js
@@ -16,28 +16,57 @@
 // const h3=document.querySelector(".heading-three")
 // console.dir(h3)
 
-const domElement=document.querySelector("#domObj")
-console.dir(domElement)
-console.log(domElement.id);
-console.log(domElement.classList);
-console.log(domElement.className);
-console.log(domElement.title);
-console.log(domElement.style);
-domElement.style.color = "blue"
-domElement.style.fontSize = "50px"
-console.log(domElement.style.color);
+// const domElement=document.querySelector("#domObj")
+// console.dir(domElement)
+// console.log(domElement.id);
+// console.log(domElement.classList);
+// console.log(domElement.className);
+// console.log(domElement.title);
+// console.log(domElement.style);
+// domElement.style.color = "blue"
+// domElement.style.fontSize = "50px"
+// console.log(domElement.style.color);
 
-const addBgColorBtn=document.querySelector("#addBgBtn")
+// const addBgColorBtn=document.querySelector("#addBgBtn")
 
-addBgColorBtn.onclick=()=>{
-    domElement.style.backgroundColor="black"
-}
+// addBgColorBtn.onclick=()=>{
+//     domElement.style.backgroundColor="black"
+// }
 
-const box=document.querySelector(".box")
-const toggleBtn=document.querySelector("#toggleBtn")
+// const box=document.querySelector(".box")
+// const toggleBtn=document.querySelector("#toggleBtn")
 
-// console.dir(box);
-toggleBtn.onclick=()=>{
-    box.classList.toggle("hidden")
-}
+// // console.dir(box);
+// toggleBtn.onclick=()=>{
+//     box.classList.toggle("hidden")
+// }
 
+// const skills=document.querySelectorAll(".btn")
+// const skillsCount=document.querySelectorAll("#count")
+// skills.forEach((skill)=>{
+//         skill.onclick=()=>{
+//             skill.classList.toggle("gray")
+//             // console.log(skill);
+//             const skillsCount=document.querySelectorAll(".btn.gray")
+//             console.log(skillsCount);
+//             count.innerText=skillsCount.length;
+//         }
+// })
+const body=document.querySelector("body")
+const heading=document.createElement("h1")
+heading.innerText="Create Element"
+body.append(heading)
+console.log(heading);
+
+const a=document.querySelector("a")
+a.href="https://youtube.com"
+console.log(a.href);
+
+console.log(a.getAttribute("href"));
+a.removeAttribute("target")
+a.setAttribute("target","-blank")
+console.log(a.getAttribute("target"));  
+
+const img=document.querySelector("img")
+console.log(img.src);
+console.log(img.getAttribute("src"));
